a432e4ff4cdc8f8172d3286109655867b3195446
Ólafur Páll Geirsson <olafurpg@gmail.com>
2018-05-22 11:36:24 +0200

    First commit

---
+++ cli/src/main/scala/tool/Main.scala
@@ -0,0 +1,35 @@
+package tool
+
+import java.nio.file.Files
+import java.nio.file.Paths
+import scala.collection.JavaConverters._
+import scala.meta.internal.{semanticdb3 => s}
+
+object Main {
+  def main(args: Array[String]): Unit = args.toList match {
+    case path :: Nil =>
+      val semanticdbRoot =
+        Paths.get(path).resolve("META-INF").resolve("semanticdb")
+      val semanticdbFiles = Files
+        .walk(semanticdbRoot)
+        .iterator()
+        .asScala
+        .filter(_.getFileName.toString.endsWith(".semanticdb"))
+        .toList
+
+      semanticdbFiles.foreach { semanticdbFile =>
+        for {
+          document <- s.TextDocuments
+            .parseFrom(Files.readAllBytes(semanticdbFile))
+            .documents
+        } {
+          pprint.log(document.uri)
+          document.occurrences.filter(_.role.isReference).foreach { o =>
+            println(o.toProtoString)
+          }
+        }
+      }
+    case els =>
+      sys.error(s"Expected <path>, obtained $els")
+  }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# SemanticDB example
+
+Quick start
+```
+git clone https://github.com/olafurpg/semanticdb-example.git
+cd semanticdb-example
+sbt
+> ~runExample
+```
+
+An example build for experimenting with analyzing Scala source code with SemanticDB.
+As of May 2018, there is no stable public Scala library interface to find, read, analyze SemanticDB data.
+This example uses `scala.meta.internal.semanticdb3`, which contains ScalaPB generated case classes.
+These classes are subject to binary and source breaking changes at any release.
+The SemanticDB protobuf payloads are guaranteed to be backwards and forwards binary compatible across every MAJOR version.
+We hope to expose a user-friendly Scala library interface to read SemanticDB data in the future.
